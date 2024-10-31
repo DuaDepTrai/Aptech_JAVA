5d8e2afb74dd8618c58307eef1c3355f649af3dc
DuaDepTrai <hieun.mgc@gmail.com>
2024-11-01 00:34:17 +0700

    update

---
+++ src/Ex_241028/BT6_2/BT6_2_1.java
@@ -0,0 +1,35 @@
+package Ex_241028.BT6_2;
+
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class BT6_2_1 {
+    public static int countWord(String s) {
+        if (s == null || s.trim().isEmpty()) {
+            return 0;
+        }
+        
+        String[] words = s.trim().split("\\s+");
+        return words.length;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s;
+        
+        System.out.println("Nhap chuoi s: ");
+        s = scanner.nextLine();
+        
+        int count = countWord(s);
+        System.out.println("The number of words in the string: " + count);
+    }
+}
