aea77d9aaeee230b7d7fa1df732a106f5df24459
DuaDepTrai <hieun.mgc@gmail.com>
2024-11-18 19:31:36 +0700

    update

---
+++ src/Exercise/Ex_241118/Student.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercise.Ex_241118;
+
+/**
+ *
+ * @author Admin
+ */
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String classRoom;
+    
+    public Student(){}
+    
+    public Student(int id, String name, int age, String classRoom) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.classRoom = classRoom;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public String getClassRoom() {
+        return classRoom;
+    }
+    
+    @Override
+    public String toString() {
+        return "ID: " + id + " - Name: " + name + " - Age: " + age + " - Class: " + classRoom;
+    }
+}
---
+++ src/Exercise/Ex_241118/StudentManager.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercise.Ex_241118;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class StudentManager {
+    private ArrayList<Student> students;
+    private Scanner scanner;
+    
+    StudentManager() {
+        students = new ArrayList<>();
+        scanner = new Scanner(System.in);
+    }
+    
+    public void addStudent() {
+        System.out.println("=====\nADD NEW STUDENT:"); 
+        System.out.print("Enter student ID: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        if (findStudentById(id) != null) {
+            System.out.println("Student ID already exists!");
+            return;
+        }
+        System.out.print("Enter student name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter student age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter student class: ");
+        String classRoom = scanner.nextLine();
+        
+        students.add(new Student(id, name, age, classRoom));
+        System.out.println("Student " + name + " was added successfully!");
+    }
+    
+    public void displayStudent() {
+        System.out.println("=====\nLIST STUDENTS: ");
+        if (students.isEmpty()) {
+            System.out.println("No student in student list.");
+        } else {
+            for (Student student : students) {
+                System.out.println(student);
+            }
+        }
+    }
+    
+    public void searchStudentById() {
+        System.out.print("Enter student ID: ");
+        int id = scanner.nextInt();
+        Student student = findStudentById(id);
+        if (student != null) {
+            System.out.println("Student information: ");
+            System.out.println(student);
+        } else {
+            System.out.println("Cannot find any student with ID: " + id);
+        }
+    }
+    
+    public void deleteStudentById() {
+        System.out.print("Enter Student ID: ");
+        int id = scanner.nextInt();
+        Student student = findStudentById(id);
+        if (student != null) {
+            students.remove(student);
+            System.out.println("Student deleted successfully!");
+        } else {
+            System.out.println("Cannot find any student with ID: " + id);
+        }
+    }
+    
+    public Student findStudentById(int id) {
+        for (Student student : students) {
+            if (student.getId() == id) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+}
